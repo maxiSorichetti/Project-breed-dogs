616ef2669bf66d8ef3d2fdac72f09a4a43cd56f7
maxisorichetti <maxisorichetti19@gmail.com>
2022-04-20 00:58:09 -0300

    deploy

--- api/index.js
+++ api/index.js
@@ -22,8 +22,9 @@ const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
 //lo paso a false para que no se reinicie cada vez que levanto el server
-conn.sync({ force: false }).then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+conn.sync({ force: true }).then(() => {
+  server.listen(process.env.PORT, () => {
+    console.log("%s listening at 3000"); // eslint-disable-line no-console
   });
 });
+
